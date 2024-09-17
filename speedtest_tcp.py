5be4bfb632b625793a051d45f7079a8ae099f62f
Lucca Giovane Gomes <lucca.giovane.gomes@uel.br>
2024-09-17 09:06:56 -0300

    lalalalal

--- speedtest_tcp.py
+++ speedtest_tcp.py
@@ -2,20 +2,23 @@ from datetime import datetime, timedelta
 from speedtest import SpeedTest
 from socket import SOCK_STREAM
 
+
 class SpeedTestTCP(SpeedTest):
 
-    def __init__(self, listen_address, connect_address, port, role):
+    def _init_(self, listen_address, connect_address, port, role):
+        # Correção: chamada correta ao super()
+
         super().__init__(listen_address, connect_address, port, role, SOCK_STREAM)
 
     def receive(self):
         received_bytes = 0
         packet = b""
 
-        while packet!=self.EMPTY_PACKET:
+        while packet != self.EMPTY_PACKET:
             packet = self.recvall(self.connection, self.PACKET_SIZE)
             received_bytes += len(packet)
-            
-         # Envia o total salvo.
+
+        # Envia o total salvo.
         stats = received_bytes.to_bytes(self.INT_BYTE_SIZE, "big", signed=False)
         self.connection.sendall(stats)
 
@@ -24,23 +27,20 @@ class SpeedTestTCP(SpeedTest):
 
         self.results(transmitted_bytes, received_bytes)
 
-    
     def send(self):
-        self.connection.listen()
-        client, _ = self.connection.accept()
-        
-        end_time = datetime.now() + timedelta(seconds=self.RUN_DURATION)
-        while datetime.now() < end_time:
-            client.sendall(self.data)
-            transmitted_bytes += len(self.data)
-        
-        client.sendall(self.EMPTY_PACKET)
+        transmitted_bytes = 0
 
-        received_bytes = self.recvall(client, self.INT_BYTE_SIZE)
+        end_time = datetime.now() + timedelta(seconds=self.DURATION)
+        while datetime.now() < end_time:
+            self.connection.sendall(self.data)
+            transmitted_bytes += len(self.data)
+
+        self.connection.sendall(self.EMPTY_PACKET)
+
+        received_bytes = self.recvall(self.connection, self.INT_BYTE_SIZE)
         received_bytes = int.from_bytes(received_bytes, "big", signed=False)
 
         stats = transmitted_bytes.to_bytes(self.INT_BYTE_SIZE, "big", signed=False)
-        client.sendall(stats)
+        self.connection.sendall(stats)
 
         self.results(transmitted_bytes, received_bytes)
-
